5b0b372b88ff56c915395bfae5b43121fae19d03
Agustin Miquelez <amiquelez@usdev.deloitte.com>
2019-04-09 10:52:50 -0500

    Categories & departments dinamically loaded

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Header.css';
 import SearchBar from '../search-bar/SearchBar';
 
-const Header = () => {
-    return (
-        <header>
-            <h1>Shopmate</h1>
-            <nav>
-                <ul>
-                    <li>Women</li>
-                    <li>Man</li>
-                    <li>Kids</li>
-                    <li>Shoes</li>
-                    <li>Brands</li>
-                </ul>
-            </nav>
-            <SearchBar />
-            <i className="fas fa-shopping-bag"></i>
-        </header>
-    );
+class Header extends Component {
+    
+    state = {
+        departments: []
+    }
+
+    componentWillMount(){
+        fetch('https://backendapi.turing.com/departments')
+        .then( res => res.json())
+        .then(data => this.setState({departments: data}))
+    }
+
+    render(){
+        const departments = this.state.departments.map(dep => {
+            return <li key={dep.department_id}>{dep.name}</li>
+        })
+        return (
+            <header>
+                <h1>Shopmate</h1>
+                <nav>
+                    <ul>{departments}</ul>
+                </nav>
+                <SearchBar />
+                <i className="fas fa-shopping-bag"></i>
+            </header>
+        )
+    }
 }
 
 export default Header;
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Menu.scss';
 
-const Menu = () => {
-    return (
-        <div className="categories_menu">
-            <h3>Categories</h3>
-            <ul>
-                <li>French</li>
-                <li>Italian</li>
-                <li>Animal</li>
-                <li>Flowers</li>
-                <li>Christmas</li>
-            </ul>
-        </div>
-    )
+class Menu extends Component {
+    
+    state = {
+        categories: []
+    }
+
+    componentWillMount() {
+        fetch('https://backendapi.turing.com/categories')
+        .then( res => res.json())
+        .then(data => this.setState({categories: data.rows}))
+    }
+
+    render(){
+        const categories = this.state.categories.map( cat => {
+            return <li key={cat.category_id}>{cat.name}</li>;
+        })
+        return (
+            <div className="categories_menu">
+                <h3>Categories</h3>
+                <ul>{categories}</ul>
+            </div>
+        )
+    }
 }
 
 export default Menu;
